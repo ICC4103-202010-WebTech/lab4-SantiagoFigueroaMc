2be9d2de448d0b2a5aa523d47b5eb3a488ca3f22
SantiagoFigueroaMc <sjfigueroa@miuandes.cl>
2020-04-09 11:58:32 -0400

    validations completed

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,9 @@
 class Customer < ApplicationRecord
   validates :name, presence: true
   validates :lastname, presence: true
-  validates :email, confirmation: { case_sensitive: false }, uniqueness: true
+  validates :email, confirmation: { case_sensitive: false }, uniqueness: true, format: { with: /\A(\S+)@(.+)\.(\S+)\z/}
 
   has_many :orders
   has_many :tickets, through: :orders
+
 end
